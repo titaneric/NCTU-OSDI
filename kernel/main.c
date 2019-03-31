9911a8b693a5f8c128167fe98114399f7e16911e
titaneric <chenyihuang001@gmail.com>
2019-03-31 14:26:15 +0800

    :penguin: Test lab4

--- kernel/main.c
+++ kernel/main.c
@@ -18,13 +18,13 @@ void kernel_main(void)
 	 kbd_init();
 	 timer_init();
 	 trap_init();
-     mem_init();
+    //  mem_init();
 
 	/* Enable interrupt */
 	__asm __volatile("sti");
 
     /* Test for page fault handler */
-    ptr = (int*)(0x12345678);
+    // ptr = (int*)(0x12345678);
     //*ptr = 1;
 
 	shell();
